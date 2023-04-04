07200bcb64a72efbbaa9c5622c0f20f9b3aa71bd
MelvilPRO <melvil.bouron@stagiairesmns.fr>
2023-04-04 09:58:42 +0200

    Création d'une map aléatoire

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var symboleDepart   = "D";// "&#x1F22F;";
 var symboleArriver  = "A";// "&#x1F232;";
 var symboleChemin   = "&#x1F201;";
 
-let graphTab = [
+var graphTab = [
     [symboleArriver, symboleObstacle, symboleCase, symboleCase, symboleCase],
     [symboleCase, symboleCase, symboleCase, symboleObstacle, symboleDepart],
     [symboleObstacle, symboleObstacle, symboleObstacle, symboleObstacle, symboleObstacle],
@@ -29,7 +29,59 @@ function findCharPos(graphTab, char){
     return result;
 }
 
+function createGraph(tailleX, tailleY, nombreObstacles){
+    let result = [];
+
+    // Remplir result de sous-tableaux contenant des cases normales
+    for (let i = 0; i < tailleY; i++){
+        let fill = [];
+        for (let k = 0; k < tailleX; k++){
+            fill[taille(fill)] = symboleCase;
+        }
+
+        result[taille(result)] = fill;
+    }
+
+    // Remplacer des cases normales aléatoirement par des obstacles
+    for (let i = 0; i < nombreObstacles; i++){
+        let posX = aleatoire(tailleX - 1, 0);
+        let posY = aleatoire(tailleY - 1, 0);
+
+        result[posY][posX] = symboleObstacle;
+    }
+
+    // Remplacer des cases normales aléatoirement par le départ et l'arrivée sans remplacer les obstacles
+    for (let i = 0; i < 2; i++){
+        let posX;
+        let posY;
+
+        // Dans le cas du départ
+        if (i == 0){
+            do {
+                posX = aleatoire(tailleX - 1, 0);
+                posY = aleatoire(tailleY - 1, 0);
+            } while(result[posY][posX] == symboleObstacle);
+
+            result[posY][posX] = symboleDepart;
+        }
+
+        // Dans le cas de l'arrivée
+        if (i == 1){
+            do {
+                posX = aleatoire(tailleX - 1, 0);
+                posY = aleatoire(tailleY - 1, 0);
+            } while(result[posY][posX] == symboleObstacle || result[posY][posX] == symboleDepart)
+
+            result[posY][posX] = symboleArriver;
+        }
+    }
+
+    return result;
+}
+
 function init(graphTab, start, end){
+    // dans le cas d'un graphique généré aléatoirement
+    graphTab = createGraph(5, 5, 10);
     afficher(graphTab);
 
     depart = findCharPos(graphTab, start);
